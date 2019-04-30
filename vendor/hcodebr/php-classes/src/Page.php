2fe273f10cdec725bc5723875d88a0380420339d
Vinicius Pinheiro <viniciuspnascimento@yahoo.com>
2019-04-29 21:33:27 -0300

    Tela de Login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -14,7 +14,7 @@ class Page {
 		"data"=>[]
 	];
 
-	public function __construct($opts = array(), $tpl_dir = "/viewa/")
+	public function __construct($opts = array(), $tpl_dir = "/views/")
 	{
 
 		$this->options = array_merge($this->defaults, $opts);
@@ -32,7 +32,7 @@ class Page {
 
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if ($this->options["header"]===true) $this->tpl->draw("header");
 
 	}
 
@@ -54,7 +54,7 @@ class Page {
 
 	public function __destruct(){
 
-		$this->tpl->draw("footer");
+		if ($this->options["footer"]===true) $this->tpl->draw("footer");
 
 	}
 
